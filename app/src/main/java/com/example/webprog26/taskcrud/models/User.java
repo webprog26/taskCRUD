830d7b3cb21ea2bd8508bf45705a43da04d788b2
webprog26 <webprog26@gmail.com>
2016-11-28 14:40:02 +0200

    User model created

---
+++ app/src/main/java/com/example/webprog26/taskcrud/models/User.java
@@ -0,0 +1,59 @@
+package com.example.webprog26.taskcrud.models;
+
+/**
+ * Created by webprog26 on 28.11.2016.
+ */
+
+public class User {
+
+    private long mUserId;
+    private String mUserName;
+    private String mUserSecondName;
+    private String mUserCity;
+
+    public long getUserId() {
+        return mUserId;
+    }
+
+    public String getUserName() {
+        return mUserName;
+    }
+
+    public String getUserSecondName() {
+        return mUserSecondName;
+    }
+
+    public String getUserCity() {
+        return mUserCity;
+    }
+
+    public static Builder newBuilder(){
+        return new User().new Builder();
+    }
+
+    public class Builder{
+        public Builder setUserId(long userId){
+            User.this.mUserId = userId;
+            return this;
+        }
+
+        public Builder setUserName(String userName){
+            User.this.mUserName = userName;
+            return this;
+        }
+
+        public Builder setUserSecondName(String userSecondName){
+            User.this.mUserSecondName = userSecondName;
+            return this;
+        }
+
+        public Builder setUserCity(String userCity){
+            User.this.mUserCity = userCity;
+            return this;
+        }
+
+        public User build(){
+            return User.this;
+        }
+    }
+}
